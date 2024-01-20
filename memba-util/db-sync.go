33bec4d4b3e9b67c4a5606d8047e3bb9910a3584
Jake Christie <antiharmonic@gmail.com>
2024-01-20 00:11:32 -0600

    stream cache v1 working

--- memba-util/db-sync.go
+++ memba-util/db-sync.go
@@ -11,6 +11,8 @@ import (
   "net/url"
   "log"
   "time"
+  // "math/rand"
+  "os"
 )
 
 var db *sql.DB
@@ -24,10 +26,19 @@ type StreamItem struct {
 
 func main() {
   // configuration/setup
+  log.SetOutput(os.Stdout)
+  configFile := os.Getenv("MEMBA_CONF")
+  if configFile == "" {
+    configFile = "/usr/src/app/config.yml"
+  }
   config.AddDriver(yaml.Driver)
-  err := config.LoadFiles("config.yml")
+  err := config.LoadFiles(configFile)
   if err != nil {
-    log.Fatal(err)
+    log.Println(err)
+    err = config.LoadFiles("./config.yml")
+    if err != nil {
+      log.Fatalln(err)
+    }
   }
   // validate db connection or bail
   connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
@@ -39,12 +50,12 @@ func main() {
   )
   db, err = sql.Open("postgres", connStr)
   if err != nil {
-    log.Fatal(err)
+    log.Fatalln(err)
   }
 
   err = db.Ping()
   if err != nil {
-    log.Fatal(err)
+    log.Fatalln(err)
   }
 
   var items []StreamItem
@@ -52,18 +63,33 @@ func main() {
   client := &http.Client{Timeout: 10 * time.Second}
   res, err := client.Get(config.String("stream.url"))
   if err != nil {
-    log.Fatal(err)
+    log.Fatalln(err)
   }
   defer res.Body.Close()
   err = json.NewDecoder(res.Body).Decode(&items)
   if err != nil {
-    log.Fatal(err)
+    log.Fatalln(err)
   }
 
   // debug only
-  fmt.Println(items[0].Title)
+  // fmt.Println(items[rand.Intn(len(items))].Title)
   // upsert existing "stream" data cache
   // i don't think this will be the final way to do this - i think ultimately we'll upsert into the mixed memba_media table or something as long as memba_media.stream_item = true or something like that
-  
+  sql := `
+    insert into stream_cache (id, title, comment, url) values ($1, $2, $3, $4)
+    on conflict (id) do update set title = $2, comment = $3, url = $4
+  `
+  sth, err := db.Prepare(sql)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer sth.Close()
 
+  for _, item := range items {
+    _, err = sth.Exec(item.Id, item.Title, item.Comment, item.Url)
+    if err != nil {
+      log.Fatalln(err)
+    }
+  }
+  log.Println("Stream data cached")
 }
